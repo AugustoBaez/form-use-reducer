b89ed38759131d8cddf28850ecb6c47db7f0cdd3
asbae <asbaez@outlook.com>
2023-01-17 22:19:32 -0300

    second commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -19,18 +19,22 @@ const Form = () => {
 
 
     const [state, dispatch] = useReducer(reducer, initialState)
+
+
     function reducer(state, action) {
         let errorAux = null
         if (action.type == "firstName" && action.payload.length < 2) errorAux = "nombre debe ser mas largo que 2 caracteres"
+        if (action.type == "lastName" && action.payload.length < 2) errorAux = "apellido debe ser mas largo que 2 caracteres"
+        // if (action.type == "email" && action.payload !== /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(('email').val())) errorAux = "email no es valido"
         return {
             ...state,
-            [action.type]: {value: action.payload, error: errorAux}
+            [action.type]: { value: action.payload, error: errorAux }
 
         }
     }
 
     function adminForm(e) {
-        const {name, value} = e.target
+        const { name, value } = e.target
         dispatch({
             type: name,
             payload: value
@@ -47,11 +51,13 @@ const Form = () => {
                 </div>
                 <div className="form">
                     <label htmlFor="firstname">Last Name:</label>
-                    <input type="text" name="lastName" id="" />
+                    <input onChange={adminForm} type="text" name="lastName" id="lastName" />
+                    {state.lastName.error !== null && (<p>{state.lastName.error}</p>)}
                 </div>
                 <div className="form">
                     <label htmlFor="firstname">Email:</label>
-                    <input type="text" name="email" id="" />
+                    <input onChange={adminForm} type="text" name="email" id="email" />
+                    {state.email.error !== null && (<p>{state.email.error}</p>)}
                 </div>
             </form>
             <button type="submit">Submit</button>
